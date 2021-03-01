16e0fa0fa9cd71a2b2b50ef8bdf0055d70d133a0
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-02-28 22:43:18 -0800

    weather test passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,65 +1,68 @@
 require('dotenv').config();
 
-const { execSync } = require('child_process');
+const { formatWeather } = require('../lib/munge-functions.js');
 
-const fakeRequest = require('supertest');
-const app = require('../lib/app');
-const client = require('../lib/client');
 
-describe('app routes', () => {
-  describe('routes', () => {
-    let token;
-  
-    beforeAll(async done => {
-      execSync('npm run setup-db');
-  
-      client.connect();
-  
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
-      token = signInData.body.token; // eslint-disable-line
-  
-      return done();
-    });
-  
-    afterAll(done => {
-      return client.end(done);
-    });
+test('gets weather from search params', async() => {
 
-    test('returns animals', async() => {
+  const expectation = [
+    {
+      'forecast': 'Overcast clouds',
+      'time': 'Sat Feb 27 2021'
+    }];
 
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+  const actualData = {
+    data: [
+      {
+        'moonrise_ts': 1614474955,
+        'wind_cdir': 'WSW',
+        'rh': 89,
+        'pres': 1014.75,
+        'high_temp': 7.8,
+        'sunset_ts': 1614477306,
+        'ozone': 365.53125,
+        'moon_phase': 0.974645,
+        'wind_gust_spd': 10.8984375,
+        'snow_depth': 0,
+        'clouds': 78,
+        'ts': 1614412860,
+        'sunrise_ts': 1614437449,
+        'app_min_temp': -1.1,
+        'wind_spd': 2.5517251,
+        'pop': 30,
+        'wind_cdir_full': 'west-southwest',
+        'slp': 1025.4375,
+        'moon_phase_lunation': 0.55,
+        'valid_date': '2021-02-27',
+        'app_max_temp': 7.8,
+        'vis': 24.096,
+        'dewpt': 3.2,
+        'snow': 0,
+        'uv': 1.924256,
+        'weather': {
+          'icon': 'c04d',
+          'code': 804,
+          'description': 'Overcast clouds'
         },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
-        }
-      ];
+        'wind_dir': 241,
+        'max_dhi': null,
+        'clouds_hi': 17,
+        'precip': 0.4375,
+        'low_temp': 2.8,
+        'max_temp': 7.8,
+        'moonset_ts': 1614441402,
+        'datetime': '2021-02-27',
+        'temp': 5,
+        'min_temp': 2.7,
+        'clouds_mid': 28,
+        'clouds_low': 78
+      }
+    ]
+  };
 
-      const data = await fakeRequest(app)
-        .get('/animals')
-        .expect('Content-Type', /json/)
-        .expect(200);
+  const data = formatWeather(actualData);
 
-      expect(data.body).toEqual(expectation);
-    });
-  });
+  expect(data).toEqual(expectation);
 });
+
+
